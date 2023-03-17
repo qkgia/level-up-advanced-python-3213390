4a6257812585e1c7ec7ed9602648cac1daeab3ba
Kha Gia Quach <khagia1001@yahoo.com>
2023-03-17 19:38:01 +0000

    Update challenge 3

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,22 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+
+
+def pairwise_offset(sequence, fillvalue='*', offset=0):
+    result = []
+    for i in range(len(sequence) + offset):
+        second_i = (i - offset)
+
+        if second_i < 0:
+            second_value = fillvalue
+        else:
+            second_value = sequence[i - offset]
+
+        if i >= len(sequence):
+            first_value = fillvalue
+        else:
+            first_value = sequence[i]
+
+        pair = tuple([first_value, second_value])
+
+        result.append(pair)
+
+    return result
